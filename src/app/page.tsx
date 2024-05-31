27071e456c306d254e6a3b88413301e5942c64d6
Roy Barneveld <roy.barneveld1@gmail.com>
2024-05-31 19:26:19 +0200

    switch name to Chess3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-4xl font-bold">ByteMe</h1>
+        <h1 className="text-4xl font-bold">Chess3</h1>
         <Web3Connect />
     </main>
   );
