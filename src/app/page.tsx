bc13841415ffaa6b7e88ff553377f5d0a7381a50
Roy Barneveld <roy.barneveld1@gmail.com>
2024-05-31 20:35:43 +0200

    Landing Page Squash
    commit 02ea7515f30a05dc398035e19aa7e059cab2e4b7
    Author: Roy Barneveld <roy.barneveld1@gmail.com>
    Date:   Fri May 31 20:35:28 2024 +0200
    
        Typewriter & Animation
    
    commit c852984e471b8dfd461588b3c4f1fbb4c2ff86db
    Author: Danaid Sinani <130862215+mrsinani@users.noreply.github.com>
    Date:   Fri May 31 14:26:32 2024 -0400
    
        Added Aurora Background to landing page
    
    commit eb1f4d2ab8c8041dc8acafbc39ffad031ce5fc3b
    Author: Roy Barneveld <roy.barneveld1@gmail.com>
    Date:   Fri May 31 20:24:34 2024 +0200
    
        AuroraComponent
    
    commit 47a112b8fbba93fca65e89df1826733acdcaef95
    Author: Danaid Sinani <130862215+mrsinani@users.noreply.github.com>
    Date:   Fri May 31 14:13:39 2024 -0400
    
        Updated Landing Page
    
    commit 27071e456c306d254e6a3b88413301e5942c64d6
    Author: Roy Barneveld <roy.barneveld1@gmail.com>
    Date:   Fri May 31 19:26:19 2024 +0200
    
        switch name to Chess3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
+'use client';
+import React from 'react';
+import { motion } from 'framer-motion';
 import Web3Connect from "@/hooks/Web3Connect";
+import { AuroraBackground } from "@/components/ui/aurora-beams";
+import { TypewriterEffect } from "@/components/ui/typewriter-effect";
 
-export default function Home() {
+const Home: React.FC = () => {
+    return (
+        <AuroraBackground>
+            <main className="flex flex-col items-center justify-between relative gap-4">
+                <TypewriterEffect
+                    words={[
+                        {
+                            text: "Chess3",
+                        },
+                    ]}
+                />
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1.5, ease: "easeIn" }}
+                >
+                    <Web3Connect />
+                </motion.div>
+            </main>
+        </AuroraBackground>
+    );
+};
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-4xl font-bold">ByteMe</h1>
-        <Web3Connect />
-    </main>
-  );
-}
+export default Home;
